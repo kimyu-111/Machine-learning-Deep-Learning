42c27f0f5602ea9663d3045ea99624d009373294
kimyu-111 <kimyou3000@naver.com>
2026-01-12 17:09:50 +0900

    Cross Validation & Grid Search

--- Cross Validation & Grid Search.py
+++ Cross Validation & Grid Search.py
@@ -6,7 +6,7 @@ target=wine['class'].to_numpy()
 
 from sklearn.model_selection import train_test_split
 
-train_input,test_input,train_target,tesst_target=train_test_split(wine,target,train_size=0.2,random_state=42)
+train_input,test_input,train_target,test_target=train_test_split(wine,target,train_size=0.2,random_state=42)
 
 sub_input,val_input,sub_target,val_target=train_test_split(train_input,train_target,test_size=0.2,random_state=42)
 
@@ -22,3 +22,58 @@ print(dt.score(val_input,val_target))
 from sklearn.model_selection import cross_validate
 scores= cross_validate(dt,train_input,train_target)
 print(scores)
+
+from sklearn.model_selection import StratifiedKFold
+scores = cross_validate(dt,train_input,train_target,cv=StratifiedKFold())
+
+import numpy as np
+print(np.mean(scores['test_score']))
+
+splitter = StratifiedKFold(n_splits=10,shuffle=True, random_state=42)
+scores= cross_validate(dt,train_input,train_target,cv=splitter)
+print(np.mean(scores['test_score']))
+
+#Grid Search
+
+from sklearn.model_selection import GridSearchCV
+params = {'min_impurity_decrease': [0.0001,0.0002,0.0003,0.0004,0.0005]}
+gs= GridSearchCV(DecisionTreeClassifier(random_state=42),params,n_jobs=-1)
+gs.fit(train_input,train_target)
+dt=gs.best_estimator_
+print(dt.score(train_input,train_target))
+print(gs.best_params_)
+print(gs.cv_results_['mean_test_score'])
+best_index = np.argmax(gs.cv_results_['mean_test_score'])
+print(gs.cv_results_['params'][best_index])
+params = {'min_impurity_decrease':np.arnage(0.0001,0.001,0.0001),'max depth':range(5,20,1),'min_smaples_split':range(2,100,10)}
+
+gs=GridSearchCV(DecisionTreeClassifier(random_state=42),params,n_jobs=-1)
+gs.fit(train_input,train_target)
+print(gs.best_params_)
+print(np.max(gs.cv_results_['mean_test_score']))
+
+#Random Search
+
+from scipy.stats import uniform, randint
+rgen=randint(0,10)
+rgen.rvs(10)
+np.unique(rgen.rvs(1000),return_counts=True)
+
+ugen= uniform(0,1)
+ugen.rvs(10)
+
+params={'min_impurity_decrease': uniform(0.0001,0.001),
+        'max_depth':randint(20,50),
+        'min_smaples_split':randint(2,25),
+        'min_smaples_leaf':randint(1,25),
+        }
+from sklearn.model_selection import RandomizedSearchCV
+gs=RandomizedSearchCV(DecisionTreeClassifier(random_state=42),params,n_iter=100,n_jobs=-1,random_state=42)
+gs.fit(train_input,train_target)
+
+print(gs.best_params_)
+
+print(np.max(gs.cv_results_['mean_test_score']))
+
+dt = gs.best_estimator_
+print(dt.score(test_input,test_target))
